b681f0d07e5f133026350b32b2e3fe453201e3f7
raffazizzi <raffaeleviglianti@gmail.com>
2015-03-31 15:54:23 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='trustyuri',
+    version='0.0.1',
+    author='TrustyURI',
+    py_modules=['trustyuri'],
+    description='Generate and check trusty URIs.'
+)
